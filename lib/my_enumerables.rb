3cf0ca89b9b2867681c75e42adbc2681bd6d4343
Mic-Tyson <mkooner@live.com>
2024-05-14 15:58:26 +0100

    Add solutions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,56 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for elem in self
+      yield elem
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    for elem in self
+      yield(elem, i)
+      i += 1
+    end
+  end
+
+  def my_all?
+    success = true
+    my_each { |elem| success = false unless yield(elem) }
+    success
+  end
+
+  def my_any?
+    success = false
+    my_each { |elem| success = true if yield(elem)}
+    success
+  end
+
+  def my_none?
+    success = true
+    my_each { |elem| success = false if yield(elem)}
+    success
+  end
+
+  def my_count
+    return length unless block_given?
+
+    count = 0
+    my_each { |elem| count += 1 if yield(elem) }
+    count
+  end
+
+  def my_map
+    mapped_array = []
+    my_each { |elem| mapped_array << yield(elem) }
+    mapped_array
+  end
+
+  def my_inject(accumulator = 0)
+    my_each { |elem| accumulator = yield(accumulator, elem) }
+    accumulator
+  end
+  
+
 end
